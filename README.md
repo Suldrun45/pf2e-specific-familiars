af6d7debba096827b12200513e757d0088e64a0a
Suldrun45 <suldrun@gmail.com>
2025-08-13 17:24:07 +0200

    Use flags to store the familiar's number of required abilities instead of a AELike

--- README.md
+++ README.md
@@ -18,7 +18,6 @@
 ### How to
 When you add a Specific Familiar ability, the number of abilities required to get that specific familiar will be deducted from the Total # of familiar abilities on the familiar's sheet via a feat added to the master's sheet.  
 If the familiar has automatic abilities (like Scent for the Corgi Mount for example), these will also be deducted from the Total # of familiar abilities.  
-The module contains a macro that you can use while selecting the token of the master of the familiar to add or remove that feat to the master's sheet.  
 
 If you use PF2e Dailies, this should allow it to show the right number dropdowns to select the remaining familiar abilities.
 
--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+const MODULE_ID = 'pf2e-specific-familiars';
+const SPECIFIC_FAMILIAR_REQUIRED_ABILITIES_UUID = "Compendium.pf2e-specific-familiars.specific-familiars-feats.Item.PN7kY8Ukw2O1WKKE";
+
 Hooks.once("init", ()=>{
 	game.pf2especificfamiliars = foundry.utils.mergeObject(game.pf2especificfamiliars ?? {}, {
         setFamiliarAbilities: setFamiliarAbilities
@@ -7,12 +10,25 @@ Hooks.once("init", ()=>{
 
 Hooks.once("ready", async () => {
 	//migration
-	await game.actors.map(p=>p).filter(p=>p.type=="character").forEach(async (act) => { 
+	await game.actors.map(p=>p).filter(p => p.type == "character").forEach(async (act) => { 
 		const OLD_ITEM_UUID = "Compendium.pf2e-specific-familiars.specific-familiars-feats.Item.YIf6rhtu23DY3zZ5";
 		const oldfeat = act.itemTypes.feat.find((e) => e._stats.compendiumSource === OLD_ITEM_UUID || e.flags.core?.sourceId === OLD_ITEM_UUID);
 		if (oldfeat){
 			await oldfeat.delete();	
 			await setFamiliarAbilities(act);
+		}		
+		const existing = act.itemTypes.feat.filter((e) => e._stats?.compendiumSource === SPECIFIC_FAMILIAR_REQUIRED_ABILITIES_UUID || e.flags.core?.sourceId === SPECIFIC_FAMILIAR_REQUIRED_ABILITIES_UUID);
+		const hasFlag = !!act.getFlag(MODULE_ID, 'familiarAbilities');		
+		if ((existing?.length ?? 0) > 0 && !hasFlag){	//remove the feat if it is not required anymore (mostly due to the new version)
+			for (const feat of existing){
+				if (feat.isOwner && feat.parent.isOwner){
+					await feat.delete();
+				}
+			}			
+			await setFamiliarAbilities(act); //Add the new version if required
+		}
+		if ((existing?.length ?? 0) == 0 && hasFlag) {
+			await setFamiliarAbilities(act); //In case the feat was deleted;
 		}
 	});
 });
@@ -33,24 +49,27 @@ Hooks.on("deleteItem", async (item) => {
 	}	
 });
 
-async function setFamiliarAbilities(act){
-	const ITEM_UUID = "Compendium.pf2e-specific-familiars.specific-familiars-feats.Item.PN7kY8Ukw2O1WKKE";
-	const existing = act.itemTypes.feat.find((e) => e._stats?.compendiumSource === ITEM_UUID || e.flags.core?.sourceId === ITEM_UUID);
-	if (existing && existing.isOwner && existing.parent.isOwner){
-		await existing.delete();
+async function setFamiliarAbilities(master){
+	if (!master)
+		return;
+	const existing = master.itemTypes.feat.find((e) => e._stats?.compendiumSource === SPECIFIC_FAMILIAR_REQUIRED_ABILITIES_UUID || e.flags.core?.sourceId === SPECIFIC_FAMILIAR_REQUIRED_ABILITIES_UUID);
+	const familiar = master.familiar;
+	const requiredNumberOfAbilities = familiar?.flags.pf2e.specificFamiliars?.requiredNumberOfAbilities ?? 0;
+	const grantedNumberOfAbilities = familiar?.flags.pf2e.specificFamiliars?.grantedNumberOfAbilities ?? 0;	
+	
+	if (requiredNumberOfAbilities > 0 || grantedNumberOfAbilities > 0){
+		await master.setFlag(MODULE_ID, 'familiarAbilities', { requiredNumberOfAbilities: requiredNumberOfAbilities, grantedNumberOfAbilities: grantedNumberOfAbilities }); //set abilities on the master
+		if (familiar && !existing){ //Create the feat if it does not exist and is required
+			const itemsource = (await fromUuid(SPECIFIC_FAMILIAR_REQUIRED_ABILITIES_UUID)).toObject();
+			itemsource._stats = foundry.utils.mergeObject(itemsource._stats ?? {}, { compendiumSource: SPECIFIC_FAMILIAR_REQUIRED_ABILITIES_UUID });
+			itemsource.flags = foundry.utils.mergeObject(itemsource.flags ?? {}, { core: { sourceId: SPECIFIC_FAMILIAR_REQUIRED_ABILITIES_UUID }});			
+			await master.createEmbeddedDocuments("Item", [itemsource]);					
+		}	
 	}
-	const familiar = act.familiar;
-	if (familiar){
-		const abilities = familiar.flags.pf2e.specificFamiliars?.requiredNumberOfAbilities;
-		const grantedabilities = familiar.flags.pf2e.specificFamiliars?.grantedNumberOfAbilities;			
-		if ((abilities && abilities > 0) || (grantedabilities && grantedabilities>0)){
-			const itemsource = (await  fromUuid(ITEM_UUID)).toObject();
-			itemsource._stats = foundry.utils.mergeObject(itemsource._stats ?? {}, { compendiumSource: ITEM_UUID });
-			itemsource.flags = foundry.utils.mergeObject(itemsource.flags ?? {}, { core: { sourceId: ITEM_UUID }});
-			const rules=itemsource.system.rules;
-			rules.push({"key":"ActiveEffectLike","mode": "add", "path": "flags.pf2e.specificFamiliars.requiredNumberOfAbilities", "value": abilities, "priority":10});
-			rules.push({"key":"ActiveEffectLike","mode": "add", "path": "flags.pf2e.specificFamiliars.grantedNumberOfAbilities", "value": grantedabilities, "priority":10});
-			await act.createEmbeddedDocuments("Item", [itemsource]);		
-		}
+	else{
+		await master.unsetFlag(MODULE_ID, 'familiarAbilities'); //set abilities on the master		
+		if (existing && existing.isOwner && existing.parent.isOwner){ //Remove the feat if it isn't required anymore
+			await existing.delete();
+		}	
 	}	
 }
