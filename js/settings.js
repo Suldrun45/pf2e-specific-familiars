ba8f0fcbdec654449e93b21f6dc6482409204610
Suldrun45 <suldrun@gmail.com>
2026-07-30 13:25:03 +0200

    Add code for migrations

--- js/settings.js
+++ js/settings.js
@@ -1,14 +1,6 @@
 import { MODULE_ID } from "./consts.js";
 
 export function setupSettings() {
-  // game.settings.register(MODULE_ID, "animations-asked-to-enable", {
-  //   name: "",
-  //   hint: "",
-  //   scope: "world",
-  //   config: false,
-  //   type: Array,
-  //   default: [],
-  // });
   game.settings.register(MODULE_ID, "last-migration", {
     name: "",
     hint: "",
